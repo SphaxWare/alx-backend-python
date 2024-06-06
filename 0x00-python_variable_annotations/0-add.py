9d828906926c67791e4f3c60fa16175a19e180bb
Sphax <itssphax123@gmail.com>
2024-06-06 22:36:57 +0000

    fix doc string

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
 """ add function"""
+
+
 def add(a: float, b: float) -> float:
+    """add function"""
     return a + b
--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
 """concat function"""
+
+
 def concat(str1: str, str2: str) -> str:
+    """concat function"""
     return str1 + str2
