76d8f817a29bb5162dfb31d295d5c05113a2396d
Sphax <itssphax123@gmail.com>
2024-06-11 02:12:51 -0700

    fix 2.0

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -8,6 +8,6 @@ from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> List[int]:
+async def async_comprehension() -> List[float]:
     """async_comprehension"""
     return [number async for number in async_generator()]
