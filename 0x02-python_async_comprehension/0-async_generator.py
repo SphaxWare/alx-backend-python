984e761c90d919d3f1ff818fcfc29d402cb5dca3
Sphax <itssphax123@gmail.com>
2024-06-11 02:11:25 -0700

    fix

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,7 +5,7 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generator[int, None, None]:
+async def async_generator() -> Generator[float, None, None]:
     """async_generator"""
     for _ in range(10):
         await asyncio.sleep(1)
