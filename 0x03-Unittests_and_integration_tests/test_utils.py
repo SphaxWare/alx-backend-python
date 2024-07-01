594e86c27484169fa6455374e70627d75ba0d5cb
Sphax <itssphax123@gmail.com>
2024-07-01 12:11:22 -0700

    2. Mock HTTP calls

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,10 +3,12 @@
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map
+from utils import get_json
+from unittest.mock import patch, Mock
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """Test"""
+    """Test Nested maps"""
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
@@ -25,6 +27,27 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(KeyError) as cm:
             access_nested_map(nested_map, path)
         self.assertEqual(str(cm.exception), f"'{path[-1]}'")
+
+class TestGetJson(unittest.TestCase):
+    """Test Json"""
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    @patch('utils.requests.get')
+    def test_get_json(self, test_url, test_payload, mock_get):
+        """Test get json"""
+        # Create a mock response object with a json method
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        mock_get.return_value = mock_response
+
+        # Call get_json and check the result
+        result = get_json(test_url)
+        self.assertEqual(result, test_payload)
+
+        # Check that requests.get was called once with the test_url
+        mock_get.assert_called_once_with(test_url)
 
 
 if __name__ == "__main__":
