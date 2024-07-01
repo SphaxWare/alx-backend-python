74945fba35140cf696002a3438f29f8333a8da52
Sphax <itssphax123@gmail.com>
2024-07-01 11:57:45 -0700

    1. Parameterize a unit test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -16,6 +16,16 @@ class TestAccessNestedMap(unittest.TestCase):
         """test access"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """test exeption"""
+        with self.assertRaises(KeyError) as cm:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(cm.exception), f"'{path[-1]}'")
+
 
 if __name__ == "__main__":
     unittest.main()
